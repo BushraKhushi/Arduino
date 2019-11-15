de529de73e80dbe759d7fe46a919ac5a5b2e2b2c
BushraKhushi <53558180+BushraKhushi@users.noreply.github.com>
2019-11-15 11:31:39 +0500

    Add files via upload
    Extract Data from Serial until the terminator

--- r_terminator.ino
+++ r_terminator.ino
@@ -5,10 +5,19 @@ void setup() {
   Serial.begin(9600);
   Serial.setTimeout(10);
   Serial.println("Input Data");
-  
-  while (Serial.available()==0){}
-  rx_byte=Serial.readStringUntil('#');
-  Serial.println(rx_byte);
 }
 void loop(){
+  
+  if (Serial.available()>0){ 
+  rx_byte=Serial.readStringUntil('#');
+  Serial.println();
+  Serial.println(rx_byte);
+  Serial.print("Input Data");
+  rx_byte = "";
+  Serial.end();
   }
+  Serial.begin(9600);
+  Serial.print(".");
+  delay(1000);
+}
+  
